a8988e5028a17a4754ca97cd63d81edaa297369a
fuyuuuu <604979146@qq.com>
2016-04-07 17:16:44 +0800

    first commit for jenkins test

---
+++ src/GitTest.java
@@ -0,0 +1,13 @@
+/**
+ * Created by zhangbin on 2016/4/7.
+ */
+public class GitTest {
+
+    public void test1(){
+        System.out.println("for testing ==");
+        System.out.println("for testing ==");
+        System.out.println("for testing ==");
+        System.out.println("for testing ==");
+        System.out.println("for testing ==");
+    }
+}
